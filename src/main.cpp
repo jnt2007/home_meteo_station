c7c9c164d413a13e7a206663e804fcde5e0d5a9b
agribenko <agribenko@tycoint.com>
2021-05-25 14:38:22 +0300

    Optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <DHT_U.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <Wire.h>
+#include <BH1750.h>
 
 #define DHTTYPE    DHT22     // DHT 22 (AM2302)
 #define DHTPIN D1
@@ -14,36 +16,51 @@ const char* mqtt_user = "";
 const char* mqtt_pass = "";
 
 // Time to sleep (60 seconds):
-const int sleepTimeS = 60e6;
+const int sleepTimeS = 300e6;
 char temp[8];
 char humidity[8];
 char voltage[6];
+char lux[8];
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
+BH1750 lightMeter;
 
 void setup_wifi() {
 
   delay(10);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(50);
+  for (int i = 0; i < 70; i++) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println();
+      Serial.println(WiFi.localIP());
+      return;
+    }
     Serial.print(".");
+    Serial.print(i);
+    delay(100);
   }
-  Serial.println(WiFi.localIP());
+  Serial.println("Failed to connect to WiFi");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  ESP.deepSleep(sleepTimeS); 
 }
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
   Serial.begin(115200);
-  Serial.setTimeout(2000);
-
-  // Wait for serial to initialize.
-  while(!Serial) { }
 
   setup_wifi();
   client.setServer(mqtt_server, 1883);
+
+  // int nVoltageRaw = analogRead(A0);
+  // // float fVoltage = (float)nVoltageRaw * 0.00686;
+  // float fVoltage = (float)nVoltageRaw * 0.00486;
+  // dtostrf(fVoltage, 4, 2, voltage);
 
   dht.begin();
   sensors_event_t event;
@@ -51,11 +68,11 @@ void setup() {
   dtostrf(event.temperature, 4, 2, temp);
   dht.humidity().getEvent(&event);
   dtostrf(event.relative_humidity, 4, 2, humidity);
+  Wire.begin();
 
-  int nVoltageRaw = analogRead(A0);
-  // float fVoltage = (float)nVoltageRaw * 0.00686;
-  float fVoltage = (float)nVoltageRaw * 0.00486;
-  dtostrf(fVoltage, 4, 2, voltage);
+  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+  int iLux = lightMeter.readLightLevel();
+  dtostrf(iLux, 1, 0, lux);
 
   randomSeed(micros());
   String clientId = "ESP8266Meteo-";
@@ -63,10 +80,12 @@ void setup() {
   if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
     client.publish("stat/meteo/temperature", temp);
     client.publish("stat/meteo/humidity", humidity);
-    client.publish("stat/meteo/voltage", voltage);
+    // client.publish("stat/meteo/voltage", voltage);
+    client.publish("stat/meteo/luminance", lux);
+    client.disconnect();
   }
-  client.disconnect();
 
+  Serial.println("Going to sleep...");
   ESP.deepSleep(sleepTimeS); 
   
   // Deep sleep mode until RESET pin is connected to a LOW signal (for example pushbutton or magnetic reed switch)
